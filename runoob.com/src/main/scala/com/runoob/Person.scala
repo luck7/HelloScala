020425d1e093087fffa66a83c1715e910c07d2a4
Jeff Fox <luck7@live.cn>
2020-04-19 11:16:48 +0800

    classes-objects

---
+++ runoob.com/src/main/scala/com/runoob/Person.scala
@@ -0,0 +1,16 @@
+class Person {
+    var name = ""
+    override def toString = getClass.getName + "[name=" + name + "]"
+}
+
+class Employee extends Person {
+    var salary = 0.0
+    override def toString = super.toString + "[salary=" + salary + "]"
+}
+
+object PersonTest extends App {
+    val fred = new Employee
+    fred.name = "Fred"
+    fred.salary = 50000
+    println(fred)
+}
