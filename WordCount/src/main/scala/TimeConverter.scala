dc263e045092e122bf9ca2d11644f7e352190a7a
Jeff Fox <luck7@live.cn>
2020-04-28 22:21:31 +0800

    Time Converter

--- WordCount/src/main/scala/TimeConverter.scala
+++ WordCount/src/main/scala/TimeConverter.scala
@@ -4,26 +4,35 @@ import java.util.Date
 object TimeConverter {
 
   def main(args: Array[String]): Unit = {
-    //val tm = "2017-08-01 16:44:32"
-    val tm = "1970-01-01 00:00:00"
-    val a = timeToLong(tm)
+    val tm = "2017-08-01 16:44:32"
+    val a = timeToMs(tm)
     println(a)
 
-    val ms = "0"
-    //val ms = "1502036122000"
-    val b = timeToString(ms)
+    val ms = "1502036122000"
+    val b = msToString(ms)
     println(b)
   }
 
-  def timeToLong(tm: String): Long = {
+  /** *
+   * 转为毫秒
+   *
+   * @param tm 格式：yyyy-MM-dd HH:mm:ss
+   * @return
+   */
+  def timeToMs(tm: String): Long = {
     val fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     val dt = fm.parse(tm)
-    val aa = fm.format(dt)
     val tim: Long = dt.getTime()
     tim
   }
 
-  def timeToString(tm: String): String = {
+  /** *
+   * 转为时间字符串
+   *
+   * @param tm
+   * @return 格式：yyyy-MM-dd HH:mm:ss
+   */
+  def msToString(tm: String): String = {
     val fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     val tim = fm.format(new Date(tm.toLong))
     tim
